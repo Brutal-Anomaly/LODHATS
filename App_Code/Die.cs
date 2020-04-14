b476a796a42a0f4c56d511f11d849eec461989cf
arcanelemon <63074135+arcanelemon@users.noreply.github.com>
2020-04-14 08:14:21 -0500

    Updated Character and DabaseManager classes

---
+++ App_Code/Die.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Die
+{
+	// member functions
+	public int roll(int max) 
+	{
+		Random rand = new Random();
+		return rand.Next(0, max + 1); 
+	}
+}
---
+++ App_Code/User.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public class User
+{
+	//member data
+	private int userID;
+	private string username;
+	private string password;
+	private string firstName;
+	private string lastName;
+	private string email;
+	private bool admin;
+
+	//constructor
+	public User(int userID, string password, string username, string firstName, string lastName, string email, bool admin)
+	{
+		this.userID = userID;
+		this.password = password;
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.admin = admin;
+	}
+
+	//mutators
+	public int getUserID() 
+	{
+		return userID;
+	}
+
+	public string getUsername() 
+	{
+		return username;
+	}
+
+	public string getPassword() 
+	{
+		return password;
+	}
+
+	public string getFirstName()
+	{
+		return firstName;
+	}
+
+	public string getLastName()
+	{
+		return lastName;
+	}
+
+	public string getEmail()
+	{
+		return email;
+	}
+
+	public bool checkPassword(string password) 
+	{
+		if (this.password == password)
+			return true;
+		else
+			return false;
+	}
+
+	//member functions
+	public bool isAdmin() 
+	{
+		if (admin)
+			return true;
+        else
+			return false;
+	}
+}
